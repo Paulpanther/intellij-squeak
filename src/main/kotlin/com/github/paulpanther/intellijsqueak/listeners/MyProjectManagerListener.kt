bb608a78dfd6b287342e05710b7df21491fcceab
GitHub Action <action@github.com>
2022-10-18 19:45:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/paulpanther/intellijsqueak/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/paulpanther/intellijsqueak/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.paulpanther.intellijsqueak.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.paulpanther.intellijsqueak.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
